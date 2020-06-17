add1837eed91806044ab3a71d60f1f097f7c8b06
eddie <guochangyi@wonderfulnet.cn>
2020-06-17 14:20:25 +0800

    常用排序

--- src/test/java/com/gcy/bootwithutils/BootwithutilsApplicationTests.java
+++ src/test/java/com/gcy/bootwithutils/BootwithutilsApplicationTests.java
@@ -2,6 +2,9 @@ package com.gcy.bootwithutils;
 
 import com.gcy.bootwithutils.service.bean.BeanService;
 import com.gcy.bootwithutils.service.date.DateService;
+import com.gcy.bootwithutils.service.json.JsonService;
+import com.gcy.bootwithutils.service.number.NumberService;
+import com.gcy.bootwithutils.service.string.StringService;
 import com.gcy.bootwithutils.vo.House;
 import com.gcy.bootwithutils.vo.Person;
 import lombok.extern.slf4j.Slf4j;
@@ -11,6 +14,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.math.BigDecimal;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.Arrays;
 import java.util.Date;
 
@@ -19,10 +25,53 @@ import java.util.Date;
 @Slf4j
 public class BootwithutilsApplicationTests {
 
+    @Test
+    public void BubbleSort(){
+        int[] arr = {3,5,6,9,13,1};
+        for(int i = 0; i < arr.length; i++){
+            for(int j = arr.length - 1; j > i; j--){
+                int max;
+               if(arr[j-1] > arr[j]){
+                   max = arr[j];
+                   arr[j] = arr[i];
+                   arr[i] = max;
+               }
+            }
+            System.out.println(Arrays.toString(arr));
+        }
+    }
 
     @Test
-    public void contextLoads() throws Exception {
-        System.out.println(DateService.stampToDate("1590940800000"));
+    public void SelectSort(){
+        int[] arr = {3,5,6,9,13,1};
+        for(int i = 0; i < arr.length; i++){
+            int min = arr[i];
+            for(int j = i + 1; j < arr.length; j++){
+                if(arr[i] > arr[j]){
+                    min = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = min;
+                }
+            }
+            System.out.println(Arrays.toString(arr));
+        }
+
+    }
+
+    @Test
+    public void InsertSort(){
+        int[] arr = {13,11,6,7,1};
+        for(int i = 1; i < arr.length; i++){
+            int item = arr[i];
+            int insertPoint = i - 1;
+            //两两比较交换
+            while(insertPoint >= 0 && arr[insertPoint] > item){
+                arr[insertPoint + 1] = arr[insertPoint];
+                insertPoint--;
+            }
+            arr[insertPoint + 1] = item;
+            System.out.println(Arrays.toString(arr));
+        }
     }
 
 
