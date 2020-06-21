303fd05bc06dbe29a822e9202b720fceb5de00fb
eddie <guochangyi@wonderfulnet.cn>
2020-06-21 13:29:49 +0800

    4 kinds Reference in java

--- src/main/java/com/gcy/bootwithutils/vo/Person.java
+++ src/main/java/com/gcy/bootwithutils/vo/Person.java
@@ -1,11 +1,15 @@
 package com.gcy.bootwithutils.vo;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@ToString
 public class Person {
 
     private String name;
--- src/test/java/com/gcy/bootwithutils/BootwithutilsApplicationTests.java
+++ src/test/java/com/gcy/bootwithutils/BootwithutilsApplicationTests.java
@@ -14,6 +14,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.lang.ref.PhantomReference;
+import java.lang.ref.ReferenceQueue;
+import java.lang.ref.SoftReference;
+import java.lang.ref.WeakReference;
 import java.math.BigDecimal;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
@@ -72,6 +76,82 @@ public class BootwithutilsApplicationTests {
             arr[insertPoint + 1] = item;
             System.out.println(Arrays.toString(arr));
         }
+    }
+
+
+    /*
+     * @Author gcy
+     * @Description 强引用，最常用到，所有的new对象都是强引用，
+     * 即使是断开引用关系，内存溢出，也不会轻易回收，需要显式地弱化为null，即可被回收
+     * @Date 16:45 2020/6/19
+     * @Param []
+     * @return void
+     **/
+    @Test
+    public void StrongReference(){
+        Object o1 = new Object();
+        Object o2 = o1;
+        o1 = null;
+        System.gc();
+        System.out.println("StrongReference After GC:" +o2);
+    }
+
+    /*
+     * @Author gcy
+     * @Description 软引用，使用 java.lang.ref.SoftReference 包来声明，内存空间足够时，不会被回收，
+     * 只有当内存空间不足时才会被回收，常用来做缓存功能。
+     * @Date 16:49 2020/6/19
+     * @Param []
+     * @return void
+     **/
+    @Test
+    public void SoftReference(){
+        String str = new String("SoftReferenceStringValue");
+        ReferenceQueue<String> referenceQueue = new ReferenceQueue<>();
+        //定义软引用队列，当软引用对象被回收，就会被加入到队列中
+        SoftReference<String> softReference = new SoftReference<>(str,referenceQueue);
+        str = null;
+        System.gc();
+        System.out.println("SoftReference After GC:" +softReference.get());
+
+    }
+
+    /*
+     * @Author gcy
+     * @Description 弱引用，使用 java.lang.ref.WeakReference 包来声明，不论内存空间是否足够，都进行回收
+     * @Date 16:50 2020/6/19
+     * @Param []
+     * @return void
+     **/
+    @Test
+    public void WeakReference(){
+        Person person = new Person("eddie",'M',20, "owner");
+        ReferenceQueue<Person> referenceQueue = new ReferenceQueue<>();
+        WeakReference<Person> weakReference = new WeakReference<>(person, referenceQueue);
+        System.gc();
+        //弱引用被清理后会立刻储存到关联的弱引用队列中
+        System.out.println("WeakReference After GC:" +weakReference.get());
+        //转为强引用继续使用
+        Person newPerson = weakReference.get();
+        System.out.println("Exchange WeakReference to StrongReference " + newPerson);
+    }
+
+    /*
+     * @Author gcy
+     * @Description 虚引用 随时可能被回收 必须跟随引用队列一起使用 主要用于跟踪对象被垃圾回收的活动
+     * @Date 13:21 2020/6/21
+     * @Param []
+     * @return void
+     **/
+    @Test
+    public void PhantomReference(){
+        ReferenceQueue queue = new ReferenceQueue();
+        String str = new String("123");
+        PhantomReference phantomReference = new PhantomReference(str,queue);
+        System.out.println("PhantomReference Before GC:" + phantomReference.get());
+        System.gc();
+        //回收后 虚引用对象加入队列中
+        System.out.println("PhantomReference After GC:" + phantomReference);
     }
 
 
